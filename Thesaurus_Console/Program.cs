eef1c5125c1bed55bd5f2fb841a630c11b49f91b
Anders Johnsson <ronnebyanders@hotmail.com>
2022-01-12 23:06:01 +0100

    Fixed better implementation of void Main on Program.cs

--- Thesaurus_Console/Program.cs
+++ Thesaurus_Console/Program.cs
@@ -13,44 +13,31 @@ namespace Thesaurus_Console
         private static List<string> Syns;
 
 
-        private static void writeWords(List<string>? wordList,int type)
+        private static void writeWords(List<string>? wordList,int type,string words="")
         {
             if (wordList != null)
             {
+                string header = "";
+
+                if (type == 0)  // AllWords
+                {
+                    header = "-------------All Words-------------------";
+                }
+                else if (type == 1)   // Synonymes
+                {
+                    header = "------------- Synonymes for " + words + "-------------------";
+                }
+
+                Console.WriteLine();
+                Console.WriteLine(header);
+
                 if (wordList.Count == 0)
                 {
-                    string headerZeroCount = "";
-
-                    if (type == 0)  // AllWords
-                    {
-                        headerZeroCount = "-------------All Words-------------------";
-                    }
-                    else if (type == 1)   // Synonymes
-                    {
-                        headerZeroCount = "------------- Synonymes-------------------";
-                    }
-
-                    Console.WriteLine();
-                    Console.WriteLine(headerZeroCount);
                     Console.WriteLine("No Words ...");
                     Console.WriteLine("-----------------------------------------");
                 }
                 else
-                {
-                    string header="";
-                    
-
-                    if (type == 0)  // AllWords
-                    {
-                        header = "-------------All Words-------------------";
-                    }
-                    else if (type == 1)   // Synonymes
-                    {
-                        header = "-------------Synonymes for " + wordList[0] + "-------------------";
-                    }
-
-                    Console.WriteLine();
-                    Console.WriteLine(header);
+                {                 
 
                     foreach (var w in wordList)
                     {
@@ -68,42 +55,39 @@ namespace Thesaurus_Console
             }
         }
 
-        private static void writeWordsWithListIDs()
+        private static void writeAllWordsToConsole()
         {
-
-            //if (AllWords.Count == 0)
-            //{
-            //    Console.WriteLine("No Words Yet...");
-            //}
-            //else
-            //{
-            //    foreach (var w in AllWords)
-            //    {
-                    
-            //    }
-            //}
+            AllWords = (List<string>)bl.GetWords();
+            writeWords(AllWords, 0);
         }
+
+        private static void writeAllSynonymesToConsole(string word)
+        {
+            Syns = (List<string>)bl.GetSynonyms(word);
+            writeWords(Syns, 1, word);
+        }
+
+
 
         static void Main(string[] args)
         {
 
-            AllWords = (List<string>)bl.GetWords();
-            writeWords(AllWords,0);
+            writeAllWordsToConsole();
 
-            Syns = (List<string>)bl.GetSynonyms("Trevlig");
-            writeWords(Syns, 1);
+            writeAllSynonymesToConsole("Trevlig");
+
 
             var newSyns = new List<string> { "Agent", "Spion", "JamesBond-typ" };
             bl.AddSynonyms(newSyns);
 
-            AllWords = (List<string>)bl.GetWords();
-            writeWords(AllWords, 0);
 
-            Syns = (List<string>)bl.GetSynonyms("spion");
-            writeWords(Syns, 1);
+            writeAllWordsToConsole();      
 
-            AllWords = (List<string>)bl.GetWords();
-            writeWords(AllWords, 0);
+            writeAllSynonymesToConsole("JamesBond-typ");
+
+            writeAllWordsToConsole();
+
+           
         }
     }
 }
